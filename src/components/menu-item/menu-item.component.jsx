2cd50e5ce2ecb56a694b6f9086a997a0615b7d4e
Kumar Sumit <kumarsumit@Kumars-MacBook-Air.local>
2021-05-21 21:56:05 +0530

    Update our menu items to naviagte withRouter HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './menu-item.component.scss';
+import { withRouter } from 'react-router-dom';
 
-const MenuItem = ({ title, imageUrl, size }) => (
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
 
-    <div className={`${size} menu-item`}>
-        <div className='background-image'
+    <div className={`${size} menu-item`}
+        onClick={() => history.push(`${match.url}${linkUrl}`)}>
+        <div
+            className='background-image'
             style={{
                 backgroundImage: `url(${imageUrl})`
             }} />
@@ -15,7 +18,7 @@ const MenuItem = ({ title, imageUrl, size }) => (
     </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
 
 
 
