9fcc27e3357e2ef3e4a502d4b3dd05712513a5c7
Kumar Sumit <kumarsumit@Kumars-MacBook-Air.local>
2021-05-22 18:28:33 +0530

    Finishing sign in handle submit method

--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -20,7 +20,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
     const snapShot = await userRef.get();
 
-    console.log(snapShot);
+    // console.log(snapShot);
 
     if (!snapShot.exists) {
         const { displayName, email } = userAuth;
