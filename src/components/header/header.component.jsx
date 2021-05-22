3e9fc412b94f03f588aa211d55c0f83356830a9b
Kumar Sumit <kumarsumit@Kumars-MacBook-Air.local>
2021-05-22 16:33:38 +0530

    Integrated Firebase Sign In, Created Sign in Page and Sign In with Google

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import './header.styles.scss';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { auth } from '../../firebase/firebase.util';
 
-const HeaderComponent = () => (
+const HeaderComponent = ({ currentUser }) => (
 
     <div className='header'>
 
@@ -17,6 +18,12 @@ const HeaderComponent = () => (
             <Link className='option' to='/shop'>
                 CONTACT
             </Link>
+            {
+                currentUser ?
+                    <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+                    :
+                    <Link className='option' to='/signin'>SIGN IN</Link>
+            }
         </div>
     </div>
 )
---
+++ src/firebase/firebase.util.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyD00MIAEG9ECL188k-8M6DlUqPxTOkHACU",
+    authDomain: "crown-db-946f5.firebaseapp.com",
+    projectId: "crown-db-946f5",
+    storageBucket: "crown-db-946f5.appspot.com",
+    messagingSenderId: "507418527626",
+    appId: "1:507418527626:web:afa23dcc64ff037fedea73",
+    measurementId: "G-3JXH6Y6WK3"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
+
